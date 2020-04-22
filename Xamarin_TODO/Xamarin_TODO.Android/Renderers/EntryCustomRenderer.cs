87352059ccd7bb389697286ac7baa5464009a12a
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2020-04-21 22:21:09 -0300

    Adicionando arquivos do projeto

---
+++ Xamarin_TODO/Xamarin_TODO.Android/Renderers/EntryCustomRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin_TODO.Controls;
+using Xamarin_TODO.Droid.Renderers;
+using static Android.Arch.Core.Internal.SafeIterableMap;
+
+
+[assembly: ExportRenderer(typeof(NoBorderEntry), typeof(EntryCustomRenderer))]
+
+namespace Xamarin_TODO.Droid.Renderers
+{
+    public class EntryCustomRenderer : EntryRenderer
+    {
+        public EntryCustomRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null || Element == null || e.OldElement != null) return;
+            var element = (NoBorderEntry)Element;
+
+            Control.SetBackground(null);
+            Control.SetPadding(0, 0, 0, 0);
+        }
+    }
+}
---
+++ Xamarin_TODO/Xamarin_TODO/ViewModel/TodoViewModel.cs
@@ -0,0 +1,48 @@
+﻿using AsyncAwaitBestPractices.MVVM;
+using MvvmHelpers;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin_TODO.Model;
+
+namespace Xamarin_TODO.ViewModel
+{
+    public class TodoViewModel : BaseViewModel
+    {
+        string description = string.Empty;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                SetProperty(ref description, value);
+            }
+        }
+        public TodoViewModel()
+        {
+            Title = "Tasks";
+            TodoTasks = new ObservableCollection<TodoTask>();
+            TodoTasks.CollectionChanged += (sender, args) =>
+              {
+                  Title = $"Tasks ({TodoTasks.Count})";
+              };
+            AddTodoTask = new AsyncCommand(AddTodo);
+           
+        }
+        public ObservableCollection<TodoTask> TodoTasks { get; set; }
+
+        public IAsyncCommand AddTodoTask { get; private set; }
+
+        async Task AddTodo()
+        {
+            await Task.Run(() => { 
+                TodoTasks.Add(new TodoTask() { Description = Description });
+            });
+        }
+
+
+    }
+}
