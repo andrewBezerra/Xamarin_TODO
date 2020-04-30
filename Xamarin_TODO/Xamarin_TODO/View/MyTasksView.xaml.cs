ec8cc19ccc2b3ddccd82709784a1f0ea0730604f
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2020-04-29 23:14:57 -0300

    Mais algumas melhorias na view.

--- Xamarin_TODO/Xamarin_TODO/View/MyTasksView.xaml.cs
+++ Xamarin_TODO/Xamarin_TODO/View/MyTasksView.xaml.cs
@@ -13,6 +13,8 @@ namespace Xamarin_TODO.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MyTasksView : ContentPage
     {
+        private bool animating = false;
+        private double lastY = 0;
         public MyTasksView()
         {
 
@@ -20,7 +22,7 @@ namespace Xamarin_TODO.View
             this.collectionView.ChildAdded +=
                 (sender, e) => collectionView_ChildAdded(sender, e);
 
-           
+
 
 
         }
@@ -32,8 +34,73 @@ namespace Xamarin_TODO.View
 
         }
 
-      
+        private async void collectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
 
 
+        }
+
+        private async Task AnimateHeader(double delta)
+        {
+            if (lastY > delta)
+            {
+
+                animating = true;
+
+                // await ImageFrame.TranslateTo(0, -400, 300, Easing.Linear);
+                await Task.WhenAll(
+                    Header.TranslateTo(0, -400, 300, Easing.Linear),
+                    Content.ScaleYTo(-75,300,Easing.Linear)
+                   
+                );
+                animating = false;
+                lastY = delta;
+
+            }
+            else
+            {
+
+                animating = true;
+                // await ImageFrame.TranslateTo(0, -400, 300, Easing.Linear);
+                await Task.WhenAll(
+                    Header.TranslateTo(0, 0, 300, Easing.Linear),
+                    Content.TranslateTo(0, 0, 300, Easing.Linear)
+                );
+                animating = false;
+                lastY = delta;
+            }
+        }
+
+        private async void SwipeGestureRecognizer_SwipedDown(object sender, SwipedEventArgs e)
+        {
+            Header.IsVisible = true;
+            animating = true;
+            await ImageFrame.TranslateTo(0, 0, 300, Easing.Linear);
+            animating = false;
+        }
+        private async void SwipeGestureRecognizer_SwipedUp(object sender, SwipedEventArgs e)
+        {
+
+            animating = true;
+            await ImageFrame.TranslateTo(0, -400, 300, Easing.Linear);
+            Header.IsVisible = false;
+            animating = false;
+        }
+
+        private async void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            if (sender is ScrollView scrollview)
+            {
+
+                if (!animating)
+                {
+
+                    await AnimateHeader(e.ScrollY);
+
+                }
+
+
+            }
+        }
     }
 }
