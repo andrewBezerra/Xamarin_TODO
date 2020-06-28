549e9500d8ca9f99e4ae3d7b44ab1d427303f914
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2020-06-27 23:35:44 -0300

    Removendo cópia do projeto

--- Xamarin_TODO/Xamarin_TODO/App.xaml.cs
+++ Xamarin_TODO/Xamarin_TODO/App.xaml.cs
@@ -2,6 +2,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin_TODO.View;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace Xamarin_TODO
 {
@@ -16,6 +19,8 @@ namespace Xamarin_TODO
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=771ce364-ee0c-419c-a9d4-75ed63691b0c;",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
