8973a4fbf7041b02e6eb4c7825cb2063e305efb7
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2020-06-28 00:07:15 -0300

    Corrigindo a estrutura do projeto

---
+++ Xamarin_TODO/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin_TODO.View;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Xamarin_TODO
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=771ce364-ee0c-419c-a9d4-75ed63691b0c;",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Xamarin_TODO/ViewModel/TodoViewModel.cs
@@ -0,0 +1,72 @@
+﻿using AsyncAwaitBestPractices.MVVM;
+using MvvmHelpers;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin_TODO.Model;
+
+namespace Xamarin_TODO.ViewModel
+{
+    public class TodoViewModel : BaseViewModel
+    {
+        string description = string.Empty;
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value);
+
+        }
+        int totaltasks = 0;
+        public int TotalTasks
+        {
+            get => totaltasks;
+            set
+            {
+                SetProperty(ref totaltasks, value);
+
+            }
+
+        }
+        public TodoViewModel()
+        {
+
+            TodoTasks = new ObservableCollection<TodoTask>();
+            TodoTasks.CollectionChanged += (sender, args) =>
+              {
+
+                  TotalTasks = TodoTasks.Count;
+              };
+
+            AddTodoTask = new AsyncCommand(AddTodo);
+            DoneTodoTask = new AsyncCommand<TodoTask>(DoneTodo);
+            RemoveTodoTask = new AsyncCommand<TodoTask>(RemoveTodo);
+
+
+        }
+        public ObservableCollection<TodoTask> TodoTasks { get; set; }
+
+        public IAsyncCommand AddTodoTask { get; private set; }
+        public IAsyncCommand<TodoTask> DoneTodoTask { get; private set; }
+        public IAsyncCommand<TodoTask> RemoveTodoTask { get; private set; }
+
+        async Task AddTodo()
+        {
+            TodoTasks.Add(new TodoTask() { Description = Description });
+            Description = string.Empty;
+        }
+        async Task RemoveTodo(TodoTask todo) => TodoTasks.Remove(todo);
+
+
+        async Task DoneTodo(TodoTask todo) => TodoTasks.First(x => x.ID == todo.ID).Done = !todo.Done;
+
+
+
+    }
+}
